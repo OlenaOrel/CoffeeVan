d5a4a8b0d60c8d3878f0e0c8b4c7cc838a78e646
= <=>
2019-12-05 15:54:26 +0200

    created view logic

---
+++ src/main/java/ua/treining/view/TextConstants.java
@@ -0,0 +1,16 @@
+package ua.treining.view;
+
+/**
+ * Created by Olena Orel on 03.12.2019.
+ */
+public interface TextConstants {
+
+    String VAN_PRODUCTS = "output.string.van.product";
+    String SORTED_VAN_PRODUCTS = "output.string.van.sorted.product";
+    String FOUND_GRAIN_COFFEE = "output.string.grain.coffee";
+    String FOUND_GROUND_COFFEE = "output.string.ground.coffee";
+    String FOUND_INSTANT_COFFEE = "output.string.instant.coffee";
+    String FOUND_PRODUCT_IN_GLASS_PACKAGE = "output.string.glass.packaging";
+    String FOUND_PRODUCT_IN_PACK = "output.string.pack";
+
+}
--- src/main/java/ua/treining/view/View.java
+++ src/main/java/ua/treining/view/View.java
@@ -1,7 +1,35 @@
 package ua.treining.view;
 
+import ua.treining.model.entity.CoffeeProduct;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  * Created by Olena Orel on 03.12.2019.
  */
 public class View {
+
+    // Resource Bundle Installation's
+    static String MESSAGES_BUNDLE_NAME = "messages";
+    public static final ResourceBundle bundle =
+            ResourceBundle.getBundle(
+                    MESSAGES_BUNDLE_NAME,
+//                    new Locale("ua", "UA"));  // Ukrainian
+                    new Locale("en"));        // English
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printProductList(List<CoffeeProduct> products) {
+        for (CoffeeProduct coffee : products) {
+            System.out.println(coffee.toString());
+        }
+    }
+
+    public void printProduct(CoffeeProduct product) {
+        System.out.println(product.toString());
+    }
 }
