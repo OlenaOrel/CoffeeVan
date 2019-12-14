3b0f274c6b0f5b89ac88183684cb9c89b9ea5b8d
= <=>
2019-12-14 14:21:17 +0200

    created CoffeeProduct

---
+++ src/main/java/ua/treining/version_2/model/entities/CoffeeProduct.java
@@ -0,0 +1,45 @@
+package ua.treining.version_2.model.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ua.treining.version_2.model.entities.coffee.Coffee;
+import ua.treining.version_2.model.entities.packaging.Packaging;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Olena Orel on 14.12.2019.
+ */
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class CoffeeProduct {
+
+    private Coffee coffee;
+    private Packaging packaging;
+    private int massOfCoffee;
+    private int productMass;
+    private BigDecimal price;
+
+    public CoffeeProduct(Coffee coffee, Packaging packaging, BigDecimal price) {
+        this.coffee = coffee;
+        this.packaging = packaging;
+        this.massOfCoffee = packaging.mass();
+        this.productMass = calculateProductMass();
+        this.price = price;
+    }
+
+    // calculate price of one kg coffee
+    public BigDecimal priceOfOneKgCoffee() {
+        return BigDecimal.valueOf((price.doubleValue() * 1000) / packaging.size());
+    }
+
+    private int calculateProductMass() {
+        return packaging.mass() + packaging.size();
+    }
+
+
+}
